6176784e3ac3ca78433c41883f2363ed6f3d040f
ashishgrewal08 <asgrewal9806@gmail.com>
2026-01-24 00:46:56 +0530

    this is change in base url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
- baseURL: "https://apnacollegeolx-backend.onrender.com/api",
+ baseURL: "https://apnacollegeolx-backend.onrender.com",
   withCredentials: true
 });
 
